13c5c3bde53d497af5f275ec751a6ee9adfe3240
Jessica Allman-LaPorte <90071482+JessicaAllman-LaPorte@users.noreply.github.com>
2023-11-01 16:43:13 -0400

    Edit DML.sql, accounts.html: accounts add and delete functionality

--- DML.sql
+++ DML.sql
@@ -1,3 +1,6 @@
+ 
+ -- ----------- TRANSACTIONS PAGE -------------- -- 
+
  -- get transactions to display on transactions.html
 SELECT date, amount, description, Accounts.account_name, Budget_categories.category_name 
 FROM Transactions
@@ -23,9 +26,30 @@ SELECT category_id, category_name
     ORDER BY category_name
 ;
 
+
+ -- ----------- ACCOUNTS PAGE -------------- --
+
  -- get accounts to display on accounts.html
 SELECT Accounts.account_id, account_name, SUM(Transactions.amount) AS account_balance 
 	FROM Accounts
 	INNER JOIN Transactions ON Accounts.account_id=Transactions.account_id 
     GROUP BY Accounts.account_id
+;
+
+ -- add accounts 
+ INSERT INTO Accounts (account_name, account_number)
+	VALUES (:account_name_input, :account_number_input)
+;
+
+ -- delete from accounts
+ DELETE FROM Accounts
+    WHERE account_id = (SELECT account_id
+                            FROM Accounts
+                            WHERE account_name = :account_name_input)
+;
+
+ -- get accounts to populate dropdown in accounts.html
+ SELECT account_id, account_name 
+    FROM Accounts
+    ORDER BY account_name
 ;
