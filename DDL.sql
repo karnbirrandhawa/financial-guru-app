c214103d77d880f5c7a2241427925a3af4beefb5
Jessica Allman-LaPorte <90071482+JessA-L@users.noreply.github.com>
2023-11-01 14:41:20 -0400

    Adds DML.sql, DDL.sql

---
+++ DDL.sql
@@ -0,0 +1,93 @@
+SET FOREIGN_KEY_CHECKS=0;
+SET AUTOCOMMIT=0;
+
+DROP TABLE IF EXISTS 
+	Transactions, Accounts, Household_members_accounts, 
+    Budget_categories, Household_members
+;
+
+CREATE TABLE Transactions (
+	transaction_id INT(11) AUTO_INCREMENT PRIMARY KEY, 
+    date DATE NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    description VARCHAR(255) NOT NULL, 
+    account_id INT(11) NOT NULL, 
+    category_id INT(11) NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES Accounts(account_id),
+    FOREIGN KEY (category_id) REFERENCES Budget_categories(category_id)
+);
+
+CREATE TABLE Budget_categories (
+	category_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(50) NOT NULL UNIQUE,
+    category_budget DECIMAL(11,2) NOT NULL 
+);
+
+CREATE TABLE Accounts (
+	account_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    account_name VARCHAR(50) NOT NULL UNIQUE,
+    account_number VARCHAR(30) 
+);
+
+CREATE TABLE Household_members_accounts (
+	household_members_accounts INT(11) AUTO_INCREMENT PRIMARY KEY,
+    account_id INT(11) NOT NULL,
+    member_id INT(11) NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES Accounts(account_id),
+    FOREIGN KEY (member_id) REFERENCES Household_members(member_id)
+);
+
+CREATE TABLE Household_members (
+	member_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    member_name VARCHAR(50) NOT NULL UNIQUE
+);
+
+INSERT INTO Transactions (date, amount, description, account_id, category_id)
+	VALUES ('2023-07-11', -27.41, 'Piggly Wiggly', 2, 2)
+;
+INSERT INTO Transactions (date, amount, description, account_id, category_id)
+    VALUES ('2023-08-21', -15, 'Bowl-O-Rama', 2, 1)
+;
+INSERT INTO Transactions (date, amount, description, account_id, category_id)
+    VALUES ('2023-08-27', -1600, 'Landlord', 3, 3)
+;
+
+INSERT INTO Budget_categories (category_name, category_budget)
+	VALUES ('Groceries', 800.00)
+;
+INSERT INTO Budget_categories (category_name, category_budget)
+	VALUES ('Entertainment', 600.00)
+;
+INSERT INTO Budget_categories (category_name, category_budget)
+	VALUES ('Rent', 800.00)
+;
+
+INSERT INTO Accounts (account_name, account_number)
+	VALUES ('Primary Checking', '023456234')
+;
+INSERT INTO Accounts (account_name)
+	VALUES ('Emergency Savings')
+;
+INSERT INTO Accounts (account_name, account_number)
+	VALUES ('House Savings', '5637973246234')
+;
+
+INSERT INTO Household_members (member_name)
+	VALUES ('Billy Joe')
+;
+INSERT INTO Household_members (member_name)
+	VALUES ('Mary Jane')
+;
+INSERT INTO Household_members (member_name)
+	VALUES ('Diana Smith')
+;
+
+INSERT INTO Household_members_accounts (account_id, member_id)
+	VALUES (1,1)
+;
+INSERT INTO Household_members_accounts (account_id, member_id)
+	VALUES (1,2)
+;
+INSERT INTO Household_members_accounts (account_id, member_id)
+	VALUES (2,1)
+;
---
+++ DML.sql
@@ -0,0 +1,31 @@
+ -- get transactions to display on transactions.html
+SELECT date, amount, description, Accounts.account_name, Budget_categories.category_name 
+FROM Transactions
+	INNER JOIN Accounts ON Transactions.account_id=Accounts.account_id
+    INNER JOIN Budget_categories ON Transactions.category_id = Budget_categories.category_id
+    ORDER BY date DESC
+;
+
+ -- add transaction in transactions.html
+INSERT INTO Transactions (date, amount, description, account_id, category_id)
+    VALUES (:date_input, :amount_input, :description_input, :account_id_input, :category_id_input)
+;
+
+ -- get account_id to populate dropdown in transactions.html
+SELECT account_id, account_name 
+    FROM Accounts
+    ORDER BY account_name
+;
+
+ -- get budget_categories to populate dropdown in transactions.html
+SELECT category_id, category_name 
+    FROM Budget_categories
+    ORDER BY category_name
+;
+
+ -- get accounts to display on accounts.html
+SELECT Accounts.account_id, account_name, SUM(Transactions.amount) AS account_balance 
+	FROM Accounts
+	INNER JOIN Transactions ON Accounts.account_id=Transactions.account_id 
+    GROUP BY Accounts.account_id
+;
