e88051f21c6afe74e0625b907cd6e2c2f757bc0c
Jessica Allman-LaPorte <90071482+JessA-L@users.noreply.github.com>
2023-11-14 11:13:01 -0500

    Edit app.py: db info comes from config file, add account number to display

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, json, redirect
 from flask_mysqldb import MySQL
 from flask import request
 import os
+import config
 
 
 app = Flask(__name__)
@@ -16,12 +17,11 @@ app = Flask(__name__)
 # app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 # database connection info
-app.config["MYSQL_HOST"] = "localhost"
-app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = ""
-app.config["MYSQL_DB"] = "bsg_universe"
-# app.config["MYSQL_DB"] = "financial_guru"
-app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+app.config["MYSQL_HOST"] = config.MYSQL_HOST
+app.config["MYSQL_USER"] = config.MYSQL_USER
+app.config["MYSQL_PASSWORD"] = config.MYSQL_PASSWORD
+app.config["MYSQL_DB"] = config.MYSQL_DB
+app.config["MYSQL_CURSORCLASS"] = config.MYSQL_CURSORCLASS
 
 mysql = MySQL(app)
 
@@ -40,6 +40,7 @@ def accounts():
     if request.method == "GET":
 
         query = "SELECT Accounts.account_id AS id, account_name AS 'Account Name', \
+                    account_number AS 'Account Number', \
                     SUM(Transactions.amount) AS 'Account Balance' \
                     FROM Accounts \
                     INNER JOIN Transactions ON Accounts.account_id=Transactions.account_id \
