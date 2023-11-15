557a5a7cab2b497ab7725b4d9d9e8578d1b317bd
Karnbir Randhawa <randhawk@oregonstate.edu>
2023-11-14 17:19:07 -0800

    added member to app & created household-members.j2

--- app.py
+++ app.py
@@ -88,8 +88,65 @@ def delete_account():
 
     return redirect("/accounts")
 
+# route for household members page
+@app.route("/household-members", methods=["POST", "GET"])
+def members():
+
+    # Grab accounts data so we send it to our template to display
+    if request.method == "GET":
+
+        query = "SELECT account_id AS id, \
+                        account_name AS 'Account Name', \
+                        account_number AS 'Account Number' \
+                    FROM Accounts"
+
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+
+        return render_template("household-members.j2", data=data)
+
+    # insert an account into the Accounts entity
+    if request.method == "POST":
+        # fire off if user presses the Add Account button
+        if request.form.get("Add_Account"):
+            # grab user form inputs
+            account_name_input = request.form["name"]
+            account_number_input = request.form["number"]
+
+            # account for null account number
+            if account_number_input == "":
+                query = "INSERT INTO Accounts (account_name) VALUES (%s)"
+                cur = mysql.connection.cursor()
+                cur.execute(query, (account_name_input))
+                mysql.connection.commit()
+
+            # no null inputs
+            else:
+                query = "INSERT INTO Accounts (account_name, account_number) \
+                            VALUES (%s, %s)"
+                cur = mysql.connection.cursor()
+                cur.execute(query, (account_name_input, account_number_input))
+                mysql.connection.commit()
+
+            # redirect back to accounts page
+            return redirect("/accounts")
+
+
+# @app.route("/delete_account", methods=["POST"])
+# def delete_account():
+
+#     if request.form.get("Delete_Account"):
+#         account_id_input = request.form["account"]
+#         query = "DELETE FROM Accounts WHERE account_id = %s;"
+#         cur = mysql.connection.cursor()
+#         cur.execute(query, (account_id_input))
+#         mysql.connection.commit()
+
+#     return redirect("/accounts")
 
 # Listener
 # change the port number if deploying on the flip servers
+# app is displaying on http://flip2.engr.oregonstate.edu:50121 when on the VPN and using above credentials  
 if __name__ == "__main__":
-    app.run(port=8126, debug=True)
+    app.run(port=50121, debug=True)
