7aafb6b20abe4a420a7d752245d0056842a4bc58
Karnbir Randhawa <randhawk@oregonstate.edu>
2023-11-14 18:20:31 -0800

    app.py, household-members.j2: READ working for members

--- app.py
+++ app.py
@@ -75,7 +75,6 @@ def accounts():
             # redirect back to accounts page
             return redirect("/accounts")
 
-
 @app.route("/delete_account", methods=["POST"])
 def delete_account():
 
@@ -88,65 +87,47 @@ def delete_account():
 
     return redirect("/accounts")
 
-# route for household members page
+# route for household-members page
 @app.route("/household-members", methods=["POST", "GET"])
 def members():
 
-    # Grab accounts data so we send it to our template to display
+    # Grab member data so we send it to our template to display
     if request.method == "GET":
 
-        query = "SELECT account_id AS id, \
-                        account_name AS 'Account Name', \
-                        account_number AS 'Account Number' \
-                    FROM Accounts"
-
+        query = "SELECT member_id AS 'id', member_name AS 'Member Name' FROM Household_members"
         cur = mysql.connection.cursor()
         cur.execute(query)
         data = cur.fetchall()
 
         return render_template("household-members.j2", data=data)
 
-    # insert an account into the Accounts entity
-    if request.method == "POST":
-        # fire off if user presses the Add Account button
-        if request.form.get("Add_Account"):
-            # grab user form inputs
-            account_name_input = request.form["name"]
-            account_number_input = request.form["number"]
+    # insert a member into the Household_members entity
+    # if request.method == "POST":
+    #     # fire off if user presses the Add Member button
+    #     if request.form.get("Add_Member"):
+    #         # grab user form inputs
+    #         member_name_input = request.form["name"]
 
-            # account for null account number
-            if account_number_input == "":
-                query = "INSERT INTO Accounts (account_name) VALUES (%s)"
-                cur = mysql.connection.cursor()
-                cur.execute(query, (account_name_input))
-                mysql.connection.commit()
+    #         # account for null member input
+    #         if member_name_input == "":
+    #             query = "INSERT INTO Household_members (member_name) VALUES (%s)"
+    #             cur = mysql.connection.cursor()
+    #             cur.execute(query, (member_name_input))
+    #             mysql.connection.commit()
 
-            # no null inputs
-            else:
-                query = "INSERT INTO Accounts (account_name, account_number) \
-                            VALUES (%s, %s)"
-                cur = mysql.connection.cursor()
-                cur.execute(query, (account_name_input, account_number_input))
-                mysql.connection.commit()
+    #         # no null inputs
+    #         else:
+    #             query = "INSERT INTO Household_members (member_name) VALUES (%s)"
+    #             cur = mysql.connection.cursor()
+    #             cur.execute(query, (member_name_input))
+    #             mysql.connection.commit()
 
-            # redirect back to accounts page
-            return redirect("/accounts")
-
-
-# @app.route("/delete_account", methods=["POST"])
-# def delete_account():
-
-#     if request.form.get("Delete_Account"):
-#         account_id_input = request.form["account"]
-#         query = "DELETE FROM Accounts WHERE account_id = %s;"
-#         cur = mysql.connection.cursor()
-#         cur.execute(query, (account_id_input))
-#         mysql.connection.commit()
-
-#     return redirect("/accounts")
+    #         # redirect back to household-members page
+    #         return redirect("/household-members")
 
 # Listener
 # change the port number if deploying on the flip servers
 # app is displaying on http://flip2.engr.oregonstate.edu:50121 when on the VPN and using above credentials  
 if __name__ == "__main__":
     app.run(port=50121, debug=True)
+ 
